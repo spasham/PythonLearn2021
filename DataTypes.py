13919e612c33d100da8119750aceb134389a7091
shivak981@gmail.com <shivak981@gmail.com>
2021-11-05 06:59:51 +0530

    updating scripts

---
+++ DataTypes.py
@@ -0,0 +1,11 @@
+#what type of data we are processing
+
+#Ex:
+
+print ("Data Types")
+total_marks = 600
+obtain_marks = int(input("plz enter your marks: "))
+percentage = (obtain_marks/total_marks)*100
+
+print ("your percentage is", percentage)
+
